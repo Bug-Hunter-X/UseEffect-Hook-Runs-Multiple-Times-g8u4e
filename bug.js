e0653bd617b084dc353054f656121132ffe0b4bf
BugHunter-X <askzenai@gmail.com>
2025-01-10 13:10:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,17 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // This effect runs only once after the component mounts
+    console.log('Component mounted!');
+  }, []);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+```
